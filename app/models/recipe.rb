78b2a9a0f17c803e7bb44690b80f42ff2a9aa1a3
Peter Jang <peter.x.jang@gmail.com>
2020-08-31 19:40:14 -0500

    Use rails generate model to make the complete model instead

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,2 @@
-class Recipe
-  attr_accessor :title, :chef, :prep_time, :ingredients, :directions
-
-  def initialize(input_options)
-    @title = input_options[:title]
-    @chef = input_options[:chef]
-    @prep_time = input_options[:prep_time]
-    @ingredients = input_options[:ingredients]
-    @directions = input_options[:directions]
-  end
+class Recipe < ApplicationRecord
 end
