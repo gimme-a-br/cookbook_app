3480cdfe389a022209ba49a1d8feb7f4624ecf1c
Peter Jang <peter.x.jang@gmail.com>
2020-09-03 19:40:00 -0500

    Rewrite index and show actions to follow RESTful conventions

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,11 +1,11 @@
 class Api::RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.first
-    render "one_recipe.json.jb"
+  def index
+    @recipes = Recipe.all
+    render "index.json.jb"
   end
 
-  def all_recipes_action
-    @all_recipes = Recipe.all
-    render "all_recipes.json.jb"
+  def show
+    @recipe = Recipe.first
+    render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/one_recipe_path" => "recipes#one_recipe_action"
-    get "/all_recipes_path" => "recipes#all_recipes_action"
+    get "/recipes" => "recipes#index"
+    get "/recipes/:id" => "recipes#show"
   end
 end
