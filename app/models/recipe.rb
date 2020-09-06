95cb3b103bdfc571884e010737c0405a46ca72f4
Peter Jang <peter.x.jang@gmail.com>
2020-09-06 11:37:12 -0500

    Add model methods to format output data

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(", ").map { |x| x.capitalize }
+  end
+
+  def directions_list
+    directions.split(", ").map { |x| x.capitalize }
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B%e, %Y")
+  end
+
+  def friendly_prep_time
+    result = ""
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
