41250c41ba643c35bc70b37e5583472bb5d4c890
Peter Jang <peter.x.jang@gmail.com>
2020-09-23 21:02:33 -0500

    Add recipes create HTML action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,20 @@ class RecipesController < ApplicationController
     render "new.html.erb"
   end
 
+  def create
+    @recipe = Recipe.new(
+      user_id: 1,
+      title: params[:title],
+      chef: params[:chef],
+      prep_time: params[:prep_time],
+      ingredients: params[:ingredients],
+      directions: params[:directions],
+      image_url: params[:image_url],
+    )
+    @recipe.save
+    redirect_to "/recipes"
+  end
+
   def show
     @recipe = Recipe.find_by(id: params[:id])
     render "show.html.erb"
